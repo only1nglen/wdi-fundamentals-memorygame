ccdaea8ece806597e90180879fc28d0d1b4a7aac
Glen Nguyen <glennguyen@Glens-MacBook-Air.local>
2019-11-09 10:29:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardOFour = "king";
+const cards = ["queen" , "queen" , "king", "king"];
 
-console.log("User flipped " + cardOne);
+const cardsInPlay = []
+
+const cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User Flipped " + cardsInPlay[0])
+
+const cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped " + cardsInPlay[1])
+
+if (cardsInPlay.length === 2) {
+	console.log("2 cards in play");
+}
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
