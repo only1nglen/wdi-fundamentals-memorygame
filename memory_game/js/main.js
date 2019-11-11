b5e31774960648249cf290d9e3f4d1b38f49461c
Glen Nguyen <glennguyen@Glens-MacBook-Air.local>
2019-11-11 10:53:25 -0500

    createdgame logic2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,34 +2,30 @@ const cards = ["queen" , "queen" , "king", "king"];
 
 const cardsInPlay = []
 
-function checkForMatch () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
-}
-
-function flipCard () {
-
-cardsInPlay.push (cards[0]);
-console.log("User flipped " + cards[0]);
-
-cardsInPlay.push (cards[2]);
-console.log("User flipped " + cards[2]);
-
-checkForMatch ();
-
-	if (cardsInPlay.length === 2) {
-	console.log("2 cards in play");
-}
+function checkForMatch() {
 
 if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+	//alert("You found a match!");
+	console.log("You found a match!");
 } else {
-	alert("Sorry, try again.");
-}
-}
+	//alert("Sorry, try again.");
+	console.log("Sorry, try again.");
+	}
+};
+
+function flipCard (cardID) {
+	cardsInPlay.push(cards[0]);
+	console.log("User flipped " + cards[0]);
+
+	cardsInPlay.push(cards[2]);
+	console.log("user flipped " + cards[2]);
+
+if (cardsInPlay.length === 2) {
+	//console.log("2 cards in play");
+	checkForMatch();
+	}
+};
+
 
 flipCard(0);
 flipCard(2);
